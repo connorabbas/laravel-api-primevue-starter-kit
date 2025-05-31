f42a73bc42c2732540adff28da9f13d9212ea0d3
Connor Abbas <abbasconnor@gmail.com>
2025-05-31 21:49:45 +0000

    eslint

--- src/components/NavLogoLink.vue
+++ src/components/NavLogoLink.vue
@@ -4,8 +4,8 @@ import ApplicationLogo from '@/components/ApplicationLogo.vue';
 
 <template>
     <Button
-        asChild
         v-slot="slotProps"
+        asChild
         variant="link"
     >
         <RouterLink
