042a17e68bcbd7274d443ad9d83d964c5a0b0435
Connor Abbas <abbasconnor@gmail.com>
2025-09-27 14:19:54 +0000

    wip: starting ts conversion

---
+++ src/main.ts
@@ -0,0 +1,43 @@
+import '@/assets/css/app.css';
+import '@/assets/css/tailwind.css';
+import 'nprogress/nprogress.css';
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
+import ConfirmationService from 'primevue/confirmationservice';
+import Tooltip from 'primevue/tooltip';
+
+import { useColorMode } from '@vueuse/core';
+import themePreset from '@/theme/noir-preset';
+import globalPt from '@/theme/global-pt';
+
+import App from '@/App.vue';
+import router from './router';
+
+const colorMode = useColorMode({ emitAuto: true });
+const app = createApp(App);
+
+app.provide('colorMode', colorMode);
+app.directive('tooltip', Tooltip);
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue, {
+    theme: {
+        preset: themePreset,
+        options: {
+            darkModeSelector: '.dark',
+            cssLayer: {
+                name: 'primevue',
+                order: 'theme, base, primevue, utilities',
+            },
+        },
+    },
+    pt: globalPt,
+});
+app.use(ConfirmationService);
+app.use(ToastService);
+
+app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,35 @@
 import { fileURLToPath, URL } from 'node:url';
+
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
+//import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from "@tailwindcss/vite";
+
 import Components from 'unplugin-vue-components/vite';
 import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-    const env = loadEnv(mode, process.cwd());
-    const devPort = parseInt(env.VITE_PORT);
+export default defineConfig(({ mode }) => {
+    // https://vite.dev/config/#using-environment-variables-in-config
+    const env = loadEnv(mode, process.cwd(), '');
+    const devPort = env.VITE_APP_PORT ? Number(env.VITE_APP_PORT) : 5173;
 
-    return defineConfig({
+    return {
         plugins: [
             vue(),
+            //vueDevTools(),
             tailwindcss(),
             Components({
-                resolvers: [PrimeVueResolver()],
-            }),
+                resolvers: [
+                    PrimeVueResolver()
+                ]
+            })
         ],
+        base: env.VITE_BASE_PATH || '/',
+        define: {
+            // Provide an explicit app-level constant derived from an env var.
+            __APP_ENV__: JSON.stringify(env.APP_ENV),
+        },
         resolve: {
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -33,5 +45,5 @@ export default ({ mode }) => {
         preview: {
             port: devPort,
         },
-    });
-};
+    };
+});
